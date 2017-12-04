dd1c99eabb4f7bed0ca5eb19bb04d77847289b8e
Arthur Barrett <abarrett@fas.harvard.edu>
2017-12-04 18:29:03 -0500

    Fixed help table formatting.

--- views/public/search/index.php
+++ views/public/search/index.php
@@ -40,9 +40,9 @@
                     <i>featured</i>, and <i>tags</i>. Examples:
                 <br><br>
                 <code>title:"Inhabited Spaces"</code><br>
-                <code>collection:Map*</code>
+                <code>collection:Map*</code><br>
                 <code>itemType:("Historical Map" OR "Still Image")</code><br>
-                <code>resulttype:Exhibit</code>
+                <code>resulttype:Exhibit</code><br>
                 <code>featured:true</code><br>
                 <code>tags:forts</code><br>
             </td>
@@ -52,14 +52,14 @@
             <td>
                 <code>paris AND fortifications</code><br>
                 <code>title:paris AND itemType:Text</code><br>
-                <code>featured:true</code>
-                <code>184?s OR 185?s</code>
+                <code>featured:true</code><br>
+                <code>184?s OR 185?s</code><br>
             </td>
         </tr>
         <tr>
             <td>Boost searches:</td>
             <td>Use the boost operator ^ to make one term more relevant than another. For example if we wanted to boost the term "paris" (the default boost is 1):<br><br>
-                <code>paris^2 western</code>
+                <code>paris^2 western</code><br>
             </td>
         </tr>
     </table>
