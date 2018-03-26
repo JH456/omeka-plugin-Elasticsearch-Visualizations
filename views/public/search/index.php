3fe6be051f0236b5b1d49454b58ac8e241f7efb4
Dilara <fdsoylu@gatech.edu>
2018-03-26 02:26:53 -0400

    switch to 2 pane search page

--- views/public/search/index.php
+++ views/public/search/index.php
@@ -1,3 +1,5 @@
+<?php if ($_SERVER['REQUEST_METHOD'] === 'GET'): ?>
+
 <?php queue_css_file('elasticsearch-results'); ?>
 <?php queue_css_url('https://www.w3schools.com/w3css/4/w3.css'); ?>
 <?php queue_css_url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'); ?>
@@ -12,60 +14,75 @@
 <?php echo head(array('title' => __('Elasticsearch'), 'bodyclass' => 'w3Page'));?>
 
 
-<div>
-    <?php echo $this->partial('search/partials/searchbar.php', array('query' => $query)); ?>
-</div>
-
-<div id="elasticsearch-help"  style="display:none;">
-    <?php echo $this->partial('search/partials/help.php'); ?>
-</div>
 
 
-<?php $totalResults = isset($results['hits']['total']) ? $results['hits']['total'].' '.__('results') : null; ?>
-<h6><?php echo __('Search') . " ($totalResults)"; ?></h6>
+<!-- SEARCH BAR -->
+<section class='w3-col l3'>
+
+    <div>
+        <?php echo $this->partial('search/partials/searchbar.php', array('query' => $query)); ?>
+    </div>
+
+    <div id="elasticsearch-help"  style="display:none;">
+        <?php echo $this->partial('search/partials/help.php'); ?>
+    </div>
+
+    <?php $totalResults = isset($results['hits']['total']) ? $results['hits']['total'].' '.__('results') : null; ?>
+    <h6><?php echo __('Search') . " ($totalResults)"; ?></h6>
+
+    <?php if($results): ?>
+        <div style="overflow:scroll; max-height:684px;" >
+            <?php if(count($results['hits']['hits']) > 0): ?>
+                <?php foreach($results['hits']['hits'] as $hit): ?>
+                    <?php echo $this->partial('search/partials/hit.php', array('hit' => $hit)); ?>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <?php echo __("Search did not return any results."); ?>
+            <?php endif; ?>
+
+            <?php echo pagination_links(); ?>
+        </div>
+    <?php else: ?>
+        <div>
+            <h2><?php echo __("Search failed"); ?></h2>
+            <p><?php echo __("The search query could not be executed. Please check your search query and try again."); ?></p>
+        </div>
+    <?php endif;  ?>
+
+</section>
 
 <!-- RESULTS -->
 <?php
 //echo "<!--".json_encode($results, JSON_PRETTY_PRINT)."-->";
 ?>
 
-<!-- Temporary Graph -->
-
-
 <!-- Search Results -->
-<?php if($results): ?>
-    <section class='w3-col l2' style="overflow:scroll; max-height:684px;">
-        <?php
-        echo $this->partial('search/partials/aggregations.php', array(
-                'query'        => $query,
-                'aggregations' => $results['aggregations'])
-        );
-        ?>
-    </section>
 
-    <section class='w3-col l4' style="overflow:scroll; max-height:684px;" >
-        <?php if(count($results['hits']['hits']) > 0): ?>
-            <?php foreach($results['hits']['hits'] as $hit): ?>
-                <?php echo $this->partial('search/partials/hit.php', array('hit' => $hit)); ?>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <?php echo __("Search did not return any results."); ?>
-        <?php endif; ?>
+<div class='w3-col l9'>
 
-        <?php echo pagination_links(); ?>
-    </section>
+    <?php if($results): ?>
+<!--
+        <section class='w3-col l2' style="overflow:scroll; max-height:684px;">
+            <?php
+            echo $this->partial('search/partials/aggregations.php', array(
+                    'query'        => $query,
+                    'aggregations' => $results['aggregations'])
+            );
+            ?>
+        </section>
+-->
+        <div>
+            <?php echo $this->partial('search/partials/graph.php'/*, array('graphData' => $graphData)*/); ?>
+        </div>
+    <?php else: ?>
+        <div>
+            <h2><?php echo __("Search failed"); ?></h2>
+            <p><?php echo __("The search query could not be executed. Please check your search query and try again."); ?></p>
+        </div>
+    <?php endif;  ?>
 
-    <section class='w3-col l6'>
-        <h3> Graph View </h3>
-        <?php echo $this->partial('search/partials/graph.php', array('graphData' => $graphData)); ?>
-    </section>
+</div>
 
+<?php echo foot(); ?>
 
-<?php else: ?>
-    <section>
-        <h2><?php echo __("Search failed"); ?></h2>
-        <p><?php echo __("The search query could not be executed. Please check your search query and try again."); ?></p>
-    </section>
-<?php endif;  ?>
-
-<?php echo foot();
+<?php endif ?>
--- views/public/search/partials/graph.php
+++ views/public/search/partials/graph.php
@@ -1,8 +1,46 @@
-<svg id='connections-graph' width='780' height='600'></svg>
+
+<svg id='connections-graph' style='width: 100%; height: 600px';></svg>
 
 <script>
-    graphVisualization.renderGraphOnSVG({
-      svgID: 'connections-graph',
-      data: <?php echo $graphData; ?>
-    });
+    (function() {
+        var appendURLParam = (url, paramName, paramVal) => {
+            if (url.indexOf('?') !== -1) {
+                return url + '&' + paramName + '=' + paramVal
+            } else {
+                return url + '?' + paramName + '=' + paramVal
+            }
+        }
+        jQuery.post(appendURLParam(window.location.href, 'graphData', 0), {}, (partialData) => {
+            var totalResults = partialData.totalResults
+            var completeData = {
+                nodeSet: new Set(partialData.nodes),
+                links: partialData.links
+            }
+            if (totalResults <= 1000) {
+                graphVisualization.renderGraphOnSVG({
+                  svgID: 'connections-graph',
+                  data: completeData
+                });
+            } else  {
+                var remainingRequests = Math.ceil((totalResults - 1000) / 1000)
+                var totalRequests = remainingRequests
+                for (var i = 1; i <= totalRequests; i++) {
+                    jQuery.post(appendURLParam(window.location.href, 'graphData', i * 1000), {}, (chunk) => {
+                        remainingRequests--;
+                        for (var j = 0; j < chunk.nodes.length; j++) {
+                            completeData.nodesSet.add(chunk.nodes[i])
+                        }
+                        completeData.links = completeData.links.concat(chunk.links)
+                        if (remainingRequests === 0) {
+                            completeData.nodes = Array.from(nodeSet)
+                            graphVisualization.renderGraphOnSVG({
+                              svgID: 'connections-graph',
+                              data: completeData
+                            });
+                        }
+                    }, 'json')
+                }
+            }
+        }, 'json')
+    }())
 </script>
