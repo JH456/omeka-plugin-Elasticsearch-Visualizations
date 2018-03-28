968a3fb2dd5c832a536f8b9aeed108265b4acf5f
Kevin Berry <kpberry11@github.com>
2018-03-27 23:40:03 -0400

    Fixes some style issues

--- views/public/search/partials/graph.php
+++ views/public/search/partials/graph.php
@@ -3,51 +3,51 @@
 
 <script>
     (function() {
-        var appendURLParam = (url, paramName, paramVal) => {
+        var appendURLParam = function(url, paramName, paramVal) {
             if (url.indexOf('?') !== -1) {
-                return url + '&' + paramName + '=' + paramVal
+                return url + '&' + paramName + '=' + paramVal;
             } else {
-                return url + '?' + paramName + '=' + paramVal
+                return url + '?' + paramName + '=' + paramVal;
             }
-        }
-        jQuery.post(appendURLParam(window.location.href, 'graphData', 0), {}, (partialData) => {
-            var totalResults = partialData.totalResults
-            var limit = partialData.limit
-            var completeData = {
+        };
+        jQuery.post(appendURLParam(window.location.href, 'graphData', 0), {}, function(partialData) {
+            var totalResults = partialData.totalResults;
+            var limit = partialData.limit;
+            var compvareData = {
                 nodes: partialData.nodes,
                 links: partialData.links
-            }
+            };
             var nodeIDSet = new Set()
             for (var i = 0; i < partialData.nodes.length; i++) {
-                nodeIDSet.add(partialData.nodes[i].id)
+                nodeIDSet.add(partialData.nodes[i].id);
             }
             if (totalResults <= limit) {
                 graphVisualization.renderGraphOnSVG({
                   svgID: 'connections-graph',
-                  data: completeData
+                  data: compvareData
                 });
             } else  {
-                var remainingRequests = Math.ceil((totalResults - limit) / limit)
-                var totalRequests = remainingRequests
+                var remainingRequests = Math.ceil((totalResults - limit) / limit);
+                var totalRequests = remainingRequests;
                 for (var i = 1; i <= totalRequests; i++) {
-                    jQuery.post(appendURLParam(window.location.href, 'graphData', i * limit), {}, (chunk) => {
+                    jQuery.post(appendURLParam(window.location.href, 'graphData', i * limit), {}, function(chunk) {
                         remainingRequests--;
                         for (var j = 0; j < chunk.nodes.length; j++) {
                             if (!nodeIDSet.has(chunk.nodes[j].id)) {
-                                nodeIDSet.add(chunk.nodes[j].id)
-                                completeData.nodes.push(chunk.nodes[j])
+                                nodeIDSet.add(chunk.nodes[j].id);
+                                compvareData.nodes.push(chunk.nodes[j]);
                             }
                         }
-                        completeData.links = completeData.links.concat(chunk.links)
+                        compvareData.links = compvareData.links.concat(chunk.links);
                         if (remainingRequests === 0) {
                             graphVisualization.renderGraphOnSVG({
                               svgID: 'connections-graph',
-                              data: completeData
+                              data: compvareData
                             });
                         }
-                    }, 'json')
+                    }, 'json');
                 }
             }
-        }, 'json')
-    }())
+        }, 'json');
+    }());
 </script>
