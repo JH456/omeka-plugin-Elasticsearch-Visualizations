b43865153c0f6c2344616d781d6ec6e186b2692c
Kevin Berry <kpberry11@gmail.com>
2018-04-22 19:04:39 -0400

    Rewrites filterMenu, updates graphFilterer filters

--- views/public/javascripts/filterMenu.js
+++ views/public/javascripts/filterMenu.js
@@ -1,191 +1,206 @@
 'use strict';
 
 var filterMenu = (function () {
-    function filter(selectedDict, completeGraph, oldFilter) {
-        var filterText = []
-        for (var tag in selectedDict) {
-            if (selectedDict[tag]) {
-                //var str = document.getElementById(tag).outerText
-                var str = tag
-                if (str != "")
-                    filterText.push(str)
+    var filter = function (filters, graph) {
+        var filterArray = [];
+        for (var category in filters) {
+            var iter = filters[category].values();
+            for (var i = 0; i < filters[category].size; i++) {
+                filterArray.push(iter.next().value);
             }
         }
-        if (oldFilter.length == filterText.length)
-            return filterText
-        graphVisualization.renderGraphOnSVG(graphFilterer.filterGraphData(filterText, completeGraph), graphColors.tagCategoryColors)
-        return filterText
-    }
-    function getColorById(id, type) {
-	var w = (id == "Box" ? " ." : ": ")
-        var color = graphColors.tagCategoryColors(id + w, type)
-	return color
-    }
-    function collapse(id, downDict) {
-	var num = id.replace("btn", "");
-        var child = document.getElementById("child" + num);
-	document.getElementById(id).setAttribute("class", "fa-li fa fa-chevron-right arrow");
-        child.style.display = "none";
-	downDict[id] = false;
-	return downDict;
-    }
-    function expand(id, downDict) {
-	var num = id.replace("btn", "");
-        var child = document.getElementById("child" + num);
-	document.getElementById(id).setAttribute("class", "fa-li fa fa-chevron-down arrow");
-        child.style.display = "block";
-	downDict[id] = true;
-	return downDict;
-    }
-    function selectParent(id, selectedDict) {
-	selectedDict[id] = true;
-	//var w = (id == "Box" ? " ." : ": ")
-        //var color = graphColors.tagCategoryColors(id + w, 'fill')
-	var color = getColorById(id, 'fill')
-        document.getElementById(id).style.backgroundColor = color;
-	return selectedDict
-    }
-    function deselectParent(id, selectedDict) {
-	selectedDict[id] = false;
-	document.getElementById(id).style.backgroundColor = "transparent";
-	return selectedDict;
-    }
-    function selectChild(id, selectedDict) {
-	if (id.startsWith("btn"))
-	    return selectedDict
-	var element = document.getElementById(id)
-	var parent = element.parentElement.parentElement
-	//selectedDict = selectParent(parent.id, selectedDict)
-	selectedDict[id] = true
-	element.style.backgroundColor = getColorById(parent.id, 'fill')
-	return selectedDict
-    }
-    function deselectChild(id, selectedDict) {
-	if (id.startsWith("btn"))
-	    return selectedDict
-	var element = document.getElementById(id)
-	var parent = element.parentElement.parentElement
-	//selectedDict = deselectParent(parent.id, selectedDict)
-	selectedDict[id] = false
-	element.style.backgroundColor = "transparent"
-	return selectedDict
-    }
-    function generateFilterMenu(tags, completeGraph) {
-        var oldFilter = []
-        var keywords = {
-	    "Box": [],
-            "Folder topic": [],
-            "Folder": [],
-            "Person": [],
-            "Organization": [],
-            "Geopolitical Entity": [],
-            "Event": [],
-            "Date": [],
-            "Facility": [],
-            "Location": [],
-            "Law": [],
+
+        var filteredGraph = graphFilterer.filterGraphData(
+            filterArray, graph
+        );
+        graphVisualization.renderGraphOnSVG(
+            filteredGraph, graphColors.getTagColor
+        );
+
+        return filters;
+    };
+
+    var toggleCategoryFilter = function (filters, baseFilters, category) {
+        if (filters[category] === undefined) {
+            filters[category] = new Set();
+        }
+
+        if (filters[category].size === baseFilters[category].size) {
+            filters[category] = new Set();
+            // filter turned off
+            return false;
+        } else {
+            // copy the set from the base filters
+            filters[category] = new Set(baseFilters[category]);
+            // filter turned on
+            return true;
+        }
+    };
+
+    var toggleTagFilter = function (filters, baseFilters, tag, category) {
+        if (filters[category] === undefined) {
+            filters[category] = new Set();
+        }
+
+        if (filters[category].has(tag)) {
+            filters[category].delete(tag);
+            // filter turned off
+            return false;
+        } else {
+            filters[category].add(tag);
+            // filter turned on
+            return true;
+        }
+    };
+
+    var getBaseFilters = function (tags, graph, categories, delimiter) {
+        var baseFilters = {};
+        for (var category in categories) {
+            baseFilters[categories[category]] = new Set();
+        }
+        baseFilters['Misc'] = new Set();
+
+        for (var tag in tags) {
+            var tagText = tags[tag].key;
+
+            var category = tagText.split(delimiter)[0].trim();
+            if (tagText.startsWith("Folder topic")) {
+                category = "Folder topic";
+            } else if (tagText.startsWith("Box") &&
+                tagText.indexOf("Folder") >= 0) {
+                category = "Folder";
+            } else if (tagText.startsWith("Box")) {
+                category = "Box";
+            }
+
+            if (baseFilters[category] === undefined) {
+                category = 'Misc';
+            }
+
+            baseFilters[category].add(tagText);
+        }
+
+        return baseFilters;
+    };
+
+    var selectTagFilterButton = function (button, category, filtersTurnedOn) {
+        var fill = "transparent";
+        if (filtersTurnedOn) {
+            fill = graphColors.getTagColor(category, "fill");
+        }
+        button.style.backgroundColor = fill;
+    };
+
+    var getTagFilterElement = function (filters, baseFilters, tag, category, delimiter, graph) {
+        var button = document.createElement("button");
+        button.classList.add("filter-button");
+        var splitTag = tag.split(delimiter);
+        splitTag = splitTag[splitTag.length - 1].split(category);
+        splitTag = splitTag[splitTag.length - 1].trim();
+        button.textContent = splitTag;
+
+        button.onclick = function () {
+            var filtersTurnedOn = toggleTagFilter(
+                filters, baseFilters, tag, category
+            );
+
+            selectTagFilterButton(button, category, filtersTurnedOn);
+
+            filter(filters, graph);
         };
 
-        var keywordIds = {}
-        for (var i = 0; i < tags.length; i++) {
-            var tag = tags[i].key;
-	    if (tag.startsWith("Box")) {
-		keywords["Box"].push(tag)
-	    }
-	    else {
-		var s = ":"
-		var parts = tag.split(s);
-		if (parts === null) {
-                    keywords["Misc"].push(tag);
-		} else if (parts[0] in keywords) {
-                    keywords[parts[0]].push(parts[1]);
-		} else {
-                    keywords["Misc"].push(parts[0]);
-		}
-	    }
-        }
-        var counter = 0;
-        var root = document.getElementById("tags");
-        var selectedDict = {};
-        var downDict = {};
-	
-        for (var word in keywords) {
+        return button;
+    }
+
+    var getCategoryFilterElement = function (
+        filters, baseFilters, category, delimiter, graph) {
+
+        var container = document.createElement("div");
+        container.classList.add("category-filter-container");
+        container.style.borderColor = graphColors.getTagColor(
+            category, "stroke"
+        );
+
+        var arrow = document.createElement("i");
+        arrow.setAttribute("class", "arrow fa-li fa fa-chevron-right");
+        container.appendChild(arrow);
+
+        var button = document.createElement("button");
+        button.classList.add("filter-button");
+        button.textContent = category;
+        container.appendChild(button);
+
+        var tagFilters = document.createElement("ul");
+        tagFilters.classList.add("tag-filter-list");
+        tagFilters.style.display = "none";
+        tagFilters.style.borderColor = container.style.borderColor;
+        container.appendChild(tagFilters);
+
+        var iter = baseFilters[category].values();
+        for (var i = 0; i < baseFilters[category].size; i++) {
+            var tag = iter.next().value;
+            var tagFilterElement = getTagFilterElement(
+                filters, baseFilters, tag, category, delimiter, graph
+            );
+
             var li = document.createElement("li");
-            var id = word
-            keywordIds[id] = word
-            li.setAttribute("id", id);
-            li.setAttribute("style", "border-style:solid;border-radius:25px;text-align:center;margin:5px;padding:4px;cursor:pointer;user-select:none;font-weight:bold")
-	    var w = (word == "Box" ? " ." : ": ")
-	    li.style.borderColor = graphColors.tagCategoryColors(word + w, 'stroke')
-	    li.style.backgroundColor = graphColors.tagCategoryColors(word + w, 'fill')
-	    
-            var i = document.createElement("i");
-            i.setAttribute("class", "fa-li fa fa-chevron-right arrow");
-            i.setAttribute("style", "top:0.5em;left:-2.0em;");
-            i.setAttribute("id", "btn" + counter.toString());
-            //arrows
-            i.addEventListener("click", function () {
-                id = event.path[0].id;
-                var down = false;
-
-                if (id in downDict) {
-                    down = downDict[id];
-                }
-                if (down) {
-		    downDict = collapse(id, downDict)
-                } else {
-		    downDict = expand(id, downDict)
-                }
-            });
-            //parent categories
-            li.addEventListener("click", function () {
-                id = event.path[0].id
-                var selected = false;
-                if (id in selectedDict) {
-                    selected = selectedDict[id]
-                }
-                if (selected) {
-		    selectedDict = (id in keywordIds ? deselectParent(id, selectedDict) : deselectChild(id, selectedDict))
-                } else {
-		    selectedDict = (id in keywordIds ? selectParent(id, selectedDict) : selectChild(id, selectedDict))
-
-                }
-		oldFilter = filter(selectedDict, completeGraph, oldFilter)
-            });
-            li.appendChild(i);
-            li.appendChild(document.createTextNode(word));
-	    //var div = document.createElement("div");
-	    //div.setAttribute("style", "overflow-y: scroll;height: 50px;")
-            var ul = document.createElement("ul");
-            ul.setAttribute("id", "child" + counter.toString());
-            ul.setAttribute("style", "display:none;list-style:none");
-            for (var i = 0; i < keywords[word].length; i++) {
-                var liTemp = document.createElement("li");
-                var subTag = keywords[word][i];
-                liTemp.appendChild(document.createTextNode(subTag));
-                liTemp.setAttribute("style", "font-weight: normal;")
-                counter += 1;
-		//selectedDict[subTag] = true
-		liTemp.setAttribute("id", subTag)
-                ul.appendChild(liTemp);
-            }
-            counter += 1;
-	    //div.appendChild(ul)
-            //li.appendChild(div);
-	    li.appendChild(ul);
-            root.appendChild(li);
+            li.appendChild(tagFilterElement);
+            tagFilters.appendChild(li);
         }
 
-        for (var word in keywords) {
-            if (keywords.hasOwnProperty(word)) {
-                selectedDict[word] = false
-                document.getElementById(word).style.backgroundColor = "transparent"
-		//document.getElementById(word).style.backgroundColor = getColorById(word, "fill")
+        arrow.onclick = function () {
+            arrow.classList.toggle("fa-chevron-right");
+            arrow.classList.toggle("fa-chevron-down");
+            if (tagFilters.style.display === "none") {
+                tagFilters.style.display = "";
+            } else {
+                tagFilters.style.display = "none";
             }
-        }
-        oldFilter = filter(selectedDict, completeGraph, oldFilter)
+        };
 
+        button.onclick = function () {
+            var filtersTurnedOn = toggleCategoryFilter(
+                filters, baseFilters, category
+            );
+
+            var fill = "transparent";
+            if (filtersTurnedOn) {
+                fill = graphColors.getTagColor(category, "fill");
+            }
+            container.style.backgroundColor = fill;
+
+            for (var i = 0; i < tagFilters.children.length; i++) {
+                selectTagFilterButton(
+                    tagFilters.children[i].children[0], category, false
+                );
+            }
+
+            filter(filters, graph);
+        };
+
+        return container;
     };
+
+    var generateFilterMenu = function (tags, graph, categories, delimiter) {
+        delimiter = delimiter || ":";
+
+        var baseFilters = getBaseFilters(
+            tags, graph, categories, delimiter
+        );
+        var filters = {};
+
+        var categoryFilterElementList = document.getElementById("tags");
+        for (var category in baseFilters) {
+            if (baseFilters[category].size !== 0) {
+                var categoryFilterElement = getCategoryFilterElement(
+                    filters, baseFilters, category, delimiter, graph
+                );
+                var li = document.createElement("li");
+                li.appendChild(categoryFilterElement);
+                categoryFilterElementList.appendChild(li);
+            }
+        }
+    }
 
     return {
         generateFilterMenu
